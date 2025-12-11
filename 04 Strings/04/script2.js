7181f060232899678ca1f4d6e07be25581bf88c3
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 20:21:58 +0700

    Add files via upload

---
+++ 04 Strings/04/script2.js
@@ -0,0 +1,13 @@
+var a = "WONDERFUL LIFE";
+var b = "GOOD LIFE"
+
+// var string = a.replace(/WONDERFUL/, b);
+
+var string = a.replace(/WONDERFUL/, "AMAZING");
+
+
+if (string !== "POWERFUL LIFE") {
+	document.write("BE NICE BE GOOD");
+} else {
+	document.write(string);
+}
