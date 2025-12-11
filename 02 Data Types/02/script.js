2343485e55c3ff8910b26fa5ab51168815d164fb
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 14:36:38 +0700

    Update script.js

--- 02 Data Types/02/script.js
+++ 02 Data Types/02/script.js
@@ -1,6 +1,15 @@
 var a = 1;
 var b = 5;
 
+//var a = 5;
+//var b = 10;
+
+//var a = 15;
+//var b = 20;
+
+//var a = 25;
+//var b = 30;
+
 if (a + b < 5 === true) {
 	document.write(1 * 1);
 } else {
