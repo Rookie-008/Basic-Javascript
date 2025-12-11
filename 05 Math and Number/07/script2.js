0b2e350d84b12e5b2be00b18f343e4a61145ced1
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 21:43:47 +0700

    Add files via upload

---
+++ 05 Math and Number/07/script2.js
@@ -0,0 +1,21 @@
+var a = prompt("Enter Number 1 :");
+var b = prompt("Enter Number 2 :");
+var c = prompt("Enter Number 3 :");
+var d = prompt("Enter Number 4 :");
+var e = prompt("Enter Number 5 :");
+
+// Integer
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+// d = parseInt(d);
+// e = parseInt(e);
+
+// Float
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+d = parseFloat(d);
+e = parseFloat(e);
+
+document.write(a + b + c + d + e);
