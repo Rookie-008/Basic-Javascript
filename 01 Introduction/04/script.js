d3bc42c4873f2b1b0c39078f8f05f3b0676df387
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 09:50:30 +0700

    Update script.js

--- 01 Introduction/04/script.js
+++ 01 Introduction/04/script.js
@@ -1,3 +1,3 @@
-document.write("<h1>Hello</h1>");
-document.write("<h2 style='color:aqua'>Hello</h2>");
-document.write("<hr>Hello</hr>");
+document.write("<h1>Something Never Changes, They Just Become Different</h1>");
+document.write("<h2 style='color:aqua'>Keep Walking, Even Small Steps Count On Dark Days</h2>");
+document.write("<hr>Life is Simple, It's Just Not Easy </hr>");
