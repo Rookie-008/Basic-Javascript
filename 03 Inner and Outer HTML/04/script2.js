5fc1783175afbca290c1fddfa1dc80203bd7920e
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 18:24:23 +0700

    Add files via upload

---
+++ 03 Inner and Outer HTML/04/script2.js
@@ -0,0 +1,8 @@
+function clickHere() {
+	var string = "Everyday Is A Chance To Get Better, Dont Waste It";
+	var a = string.replace("It's Easy To Say, But It's Not Easy To Do");
+
+	console.log(a);
+}
+
+clickHere();
