485aee4d1da9f8e8f6dd77432acd0f8cd2c57443
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 14:49:54 +0700

    Update script.js

--- 02 Data Types/05/script.js
+++ 02 Data Types/05/script.js
@@ -1,5 +1,5 @@
-var string1 = "Hello Hello";
-var string2 = "ABCDE"
+var string1 = "It Hurts Because It Matters";
+var string2 = "Life is Simple, It's Just Not Easy"
 
 // document.write(string1 + " " + string2);
 console.log(string1 + " " + string2);
