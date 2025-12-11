6c7e113fb8314646bceee3ba03c6fb3df4d138d2
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 09:57:20 +0700

    Update script.js

--- 01 Introduction/09/script.js
+++ 01 Introduction/09/script.js
@@ -1,5 +1,5 @@
 let a = "Something Never Changes, They Just Become Different";                // String
-let b = new String("Hello");  // Object
+let b = new String("Life is Simple, It's Just Not Easy");  // Object
 
 console.log(a);
 console.log(b);
