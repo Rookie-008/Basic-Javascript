afa7a7b4884e4c7b057ec89c36bfed09b530c60c
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 18:24:43 +0700

    Add files via upload

---
+++ 03 Inner and Outer HTML/05/script2.js
@@ -0,0 +1,4 @@
+function clickHere(){
+	let string = document.getElementById("paragraf").innerHTML;
+	document.getElementById("paragraf").innerHTML = string.replace("Everyday Is A Chance To Get Better, Dont Waste It", "IT`S EASY TO SAY, BUT IT'S NOT EASY TO DO");
+}
