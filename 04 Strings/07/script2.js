ee93409f35d01598db34270d5139753c818c83d5
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 20:22:49 +0700

    Add files via upload

---
+++ 04 Strings/07/script2.js
@@ -0,0 +1,11 @@
+var a = "LIFE IS WONDERFUL AND SO GOOD";
+var b = "LIFE IS SO GOOD AND WONDERFUL";
+
+var string1 = a.substring(8, 16);
+var string2 = b.substring(8, 14);
+
+if ( string1 == string2 ) {
+	document.write("BE NICE BE GOOD");
+} else {
+	document.write("LIFE IS SIMPLE, BUT IT'S NOT EASY");
+}
