d1884df69c1815f8517631af7214c21e0e4488dd
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 14:59:09 +0700

    Add files via upload

---
+++ 02 Data Types/01/script2.js
@@ -0,0 +1,14 @@
+var a = 5;
+var b = 15;
+var c = 20;
+
+//var a = 25;
+//var b = 35;
+//var c = 40;
+
+//var a = 45;
+//var b = 55;
+//var c = 60;
+
+// document.write(a + b + c);
+console.log(a + b + c);
