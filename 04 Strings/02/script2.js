a4f524be6b98983dce6d5101c71a73867158c3fc
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 20:21:22 +0700

    Add files via upload

---
+++ 04 Strings/02/script2.js
@@ -0,0 +1,12 @@
+var string1 = "LIFE IS WONDERFUL";
+var string2 = "LIFE IS SO GOOD";
+
+var a = string1.includes(string2);
+
+// Check Condition Value String1 and String2
+if (a === true) {
+	console.log(string1 + " " + string2)
+	document.write("W O N D E R F U L");
+} else {
+	console.log("Need Permission");
+}
