08a226c144b4949705fee83de76a38203a1d4388
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-11 14:33:11 +0700

    Update script.js

--- 01 Introduction/07/script.js
+++ 01 Introduction/07/script.js
@@ -7,4 +7,10 @@
 const a = 10;      // Not allowed => Variable "const" cannot be redeclared, cannot be reassigned, have block scope
 const a = 20;      // Not allowed => Variable "constt" cannot be redeclared, cannot be reassigned, have block scope
 
+//const a = 30;      // Not allowed => Variable "const" cannot be redeclared, cannot be reassigned, have block scope
+//const a = 40;      // Not allowed => Variable "constt" cannot be redeclared, cannot be reassigned, have block scope
+
+//const a = 50;      // Not allowed => Variable "const" cannot be redeclared, cannot be reassigned, have block scope
+//const a = 60;      // Not allowed => Variable "constt" cannot be redeclared, cannot be reassigned, have block scope
+
 console.log(a);
