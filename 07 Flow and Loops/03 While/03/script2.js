74b7c6345d4fbb43ac930c44298fe3438d89bfcf
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-13 20:02:28 +0700

    Add files via upload

---
+++ 07 Flow and Loops/03 While/03/script2.js
@@ -0,0 +1,9 @@
+// User Control Loop
+
+var condition = true;
+
+while(condition) {
+	console.log('No Matter How Hard The Past, You Can Always Begin Again');
+
+	condition = confirm('Create Loop?');
+}
