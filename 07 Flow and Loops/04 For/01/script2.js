e4f939df8dd3f4922e681f8f739a0d18853a1612
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-13 20:03:15 +0700

    Add files via upload

---
+++ 07 Flow and Loops/04 For/01/script2.js
@@ -0,0 +1,7 @@
+var a = 0;
+
+for ( a = 0; a <= 30; a++){
+	// console.log('No Matter How Hard The Past, You Can Always Begin Again')
+	// document.write('Whatever You Are, Be A Good One ');
+	console.log("Value " + a);
+}
