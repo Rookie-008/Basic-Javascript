bd06f4f82345219e2b80635fa67d27332b0e3d40
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-13 20:05:28 +0700

    Add files via upload

---
+++ 07 Flow and Loops/05 Switch/01/script2.js
@@ -0,0 +1,24 @@
+var a = prompt('Insert Number ( 1 - 5 ): ');
+
+switch(a){
+	case '1' :
+		alert('No Matter How Hard The Past, You Can Always Begin Again');
+		break;
+	case '2' :
+		alert('When You Get Tired, Learn To Rest, Not Quit');
+		break;
+	case '3' :
+		alert('Where There Is Love, There Is Life');
+		break;
+	case '4' :
+		alert('If Something Is Wrong, Train Yourself Not To Worry, Worry Fixes Nothing');
+		break;
+	case '5' :
+		alert('Do What Is Right, Even If You Are Flying Solo');
+		break;
+	default :
+		alert('INVALID.. Range number 1 - 5')
+		break;
+}
+
+
