d9e6d2eccafff84a4e4d0000eba3060aa3dd9447
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-13 20:04:50 +0700

    Add files via upload

---
+++ 07 Flow and Loops/04 For/05/script2.js
@@ -0,0 +1,12 @@
+var total = 30;
+var a = 0;
+
+for (a = 0; a < 30; a++){
+	if (a < 15) {
+		console.log('No Matter How Hard The Past, You Can Always Begin Again' + a);
+	} else if (a > 25){
+		console.log('When You Get Tired, Learn To Rest Not Quit ' + a);
+	} else {
+		console.log('Not Exist');
+	}
+}
---
+++ 07 Flow and Loops/04 For/05/script3.js
@@ -0,0 +1,12 @@
+var total = 50;
+var a = 0;
+
+for (a = 0; a < 50; a++){
+	if (a > 15 && a < 25) {
+		console.log('No Matter How Hard The Past, You Can Always Begin Again' + a);
+	} else if (a > 35 && a < 45){
+		console.log('When You Get Tired, Learn To Rest, Not Quit ' + a);
+	} else {
+		console.log('Not Exist');
+	}
+}
