f0901182ec4591d0d7d25e41a79f1723a0c3cfd8
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-14 14:06:47 +0700

    Add files via upload

---
+++ 07 Flow and Loops/05 Switch/02/script2.js
@@ -0,0 +1,37 @@
+var a = prompt('Insert Name: \n (Box-1, Box-2, Box-3, Box-4, Box-5, Box-6, Box-7, Box-8, Box-9, Box-10)');
+
+switch(a){
+	case 'Box-1' :
+		alert('Kerupuk');
+		break;
+	case 'Box-2' :
+		alert('Kacang Selimut');
+		break;
+	case 'Box-3' :
+		alert('Kembang Goyang');
+		break;
+	case 'Box-4' :
+		alert('Keripik');
+		break;
+	case 'Box-5' :
+		alert('Kacang Koro');
+		break;
+	case 'Box-6' :
+		alert('Kuping Gajah');
+		break;
+	case 'Box- 7' :
+		alert('Basreng');
+		break;
+	case 'Box-8' :
+		alert('Kacang Telor');
+		break;
+	case 'Box-9' :
+		alert('Kacang Polong');
+		break;
+	case 'Box-10' :
+		alert('Makaroni');
+		break;
+	default :
+		alert('Box not Available : \n Choose (Box-1, Box-2, Box-3, Box-4, Box-5, Box-6, Box-7, Box-8, Box-9, Box-10)');
+		break;
+}
