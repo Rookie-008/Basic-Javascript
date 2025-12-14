60a09e6406fb30d916b0b9844afb2d827312fa1a
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-14 20:54:21 +0700

    Add files via upload

---
+++ 08 Function/01/script2.js
@@ -0,0 +1,42 @@
+// | 1 |
+// function inspire(){
+// 	var string = "IT HURTS BECAUSE IT MATTERS";
+
+// 	document.write("<h1>Where There Is Love, There Is Life " + string + "</h1>");
+
+// 	alert("BE NICE BE GOOD");
+// }
+
+// inspire();
+
+
+
+// | 2 |
+// function inspire(string){
+
+// 	document.write("<h1>Be Your Own Kind Of Beautiful " + string + "</h1>");
+
+// }
+
+// inspire("IT HURTS BECAUSE IT MATTERS")
+// inspire("Where There Is Love, There Is Life")
+// inspire("BE NICE BE GOOD")
+
+
+
+// | 3 |
+//function inspire(string1, string2, string3) {
+//	document.write("<h1>" + string1 + "</h1>");
+//	document.write("<h1>" + string2 + "</h1>");
+//	document.write("<p>" + string3 + "<p>");
+//}
+
+//inspire("IT HURTS BECAUSE IT MATTERS","BE NICE BE GOOD", "Where There Is Love, There Is Life")
+
+// | 4 |
+function hello(name, id) {
+	document.write("<h1> Hello " + name + "</h1>");
+	document.write("<p> Hello " + id + "<p>");
+}
+
+hello("Bruce Lee", 41614463)
