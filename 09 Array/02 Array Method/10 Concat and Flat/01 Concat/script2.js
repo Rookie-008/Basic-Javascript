35c9bd2f159ad286649fbeb66c6871dc69e635cf
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 14:07:18 +0700

    Add files via upload

---
+++ 09 Array/02 Array Method/10 Concat and Flat/01 Concat/script2.js
@@ -0,0 +1,8 @@
+var id = [0,1,2,3,5,6,7,8,9];
+var minuman = ['Cokelat Hangat','Cendol','Es Buah', 'Es Cincau','Limun Hangat'];
+var cemilan = ['Basreng','Kacang Atom','Seblak','Makaroni','Kacang Bawang','Kacang Telur','Kerupuk Kulit','Mie Lidi','Kuping Gajah','Sakura Pedas'];
+
+var showArray = id.concat(minuman, cemilan);
+
+console.log(showArray);
+console.log(showArray.join(' - '));
