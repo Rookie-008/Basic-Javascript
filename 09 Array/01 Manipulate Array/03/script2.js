1d3786e2004daeba1d5de06583fe652cf33a6967
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 14:00:05 +0700

    Add files via upload

---
+++ 09 Array/01 Manipulate Array/03/script2.js
@@ -0,0 +1,5 @@
+// Show Array
+var cemilan = ["Keripik Singkong", "Basreng", "Keripik Sukun", "Makaroni", "Kacang Mede", "Kacang Atom"];
+for (var i = 0; i < cemilan.length; i++){
+	console.log('Aneka Cemilan Pedas Dan Nikmat ' + (i + 1) + ' -> ' + array[i]);
+}
