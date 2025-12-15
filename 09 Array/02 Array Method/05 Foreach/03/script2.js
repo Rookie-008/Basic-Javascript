ebd96ec2283dcb5ea18efcc71c4335c81d542507
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 14:03:10 +0700

    Add files via upload

---
+++ 09 Array/02 Array Method/05 Foreach/03/script2.js
@@ -0,0 +1,13 @@
+var id = [0,1,2,3,4,5,6,7,8,9];
+var cemilan = ['Basreng','Kacang Atom','Seblak','Makaroni','Kacang Bawang','Kacang Telur','Kerupuk Kulit','Mie Lidi','Kuping Gajah','Sakura Pedas'];
+
+id.forEach(function(element, indeks) {
+	console.log('Indeks ' + (indeks + 1) + ' -> ' + element)
+});
+
+cemilan.forEach(function(element, indeks) {
+	console.log('Indeks ' + (indeks + 1) + ' -> ' + element)
+});
+
+// Note :
+// foreach -> Looping Array
