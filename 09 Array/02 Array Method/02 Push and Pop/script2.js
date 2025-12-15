ff9853cf074d113f05e3149d77b39d9fe0b32905
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 14:00:46 +0700

    Add files via upload

---
+++ 09 Array/02 Array Method/02 Push and Pop/script2.js
@@ -0,0 +1,12 @@
+var cemilan = ['Kacang Atom', 'Makaroni', 'Basreng', 'Kacang Mede', 'Kuping Gajah'];
+// cemilan.push('Keripik Kentang');
+cemilan.pop();
+cemilan.pop();
+cemilan.pop();
+
+console.log(cemilan.join(' - '));
+
+
+// NOTE :
+// push -> Add Element on Last Array
+// pop  -> Remove Element on Last Array
