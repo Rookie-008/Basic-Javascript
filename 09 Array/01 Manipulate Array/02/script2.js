4b4f7760b9db1fe5fe4b6e12d89b506293afd846
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 13:59:45 +0700

    Add files via upload

---
+++ 09 Array/01 Manipulate Array/02/script2.js
@@ -0,0 +1,20 @@
+// 1. Add Array
+// var cemilan = [];
+// cemilan[0] = "Kacang Bawang";
+// cemilan[1] = "Keripik Sukun";
+// cemilan[2] = "Kacang Mede";
+// cemilan[3] = "Keciput Gurih";
+// cemilan[4] = "Kacang Atom";
+
+// console.log(array);
+
+// 2. Delete Array
+// var cemilan = ["Kacang Atom", "Kacang Mede", "Keripik Sukun", "Kacang Bawang", "Keciput Gurih"];
+// cemilan[4] = undefined;
+// console.log(cemilan);
+
+// 3. Show Array
+var cemilan = ["Makaroni", "Basreng", "Seblak", "Keripik Kentang", "Keripik Singkong"];
+for (var i = 0; i < 5; i++){
+	console.log(cemilan[i]);
+}
