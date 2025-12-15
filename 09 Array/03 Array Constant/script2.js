e78ec79c731f4ccf6ae6d5065305f785a3520577
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 14:08:37 +0700

    Add files via upload

---
+++ 09 Array/03 Array Constant/script2.js
@@ -0,0 +1,14 @@
+const minuman = ["Cokelat Hangat", "Jahe Hangat", "Limun Hangat", "Jus Alpukat", "Es Cendol", "Jus Melon","Es Buah","Jus Jeruk","Es Kelapa"];
+
+console.log(minuman);
+
+// Change Element
+minuman[0] = "Es Cincau";
+console.log(minuman);
+
+// Add Array
+minuman.push("Es Cincau");
+console.log(minuman);
+
+// Show Array
+console.log(minuman.join(' '));
