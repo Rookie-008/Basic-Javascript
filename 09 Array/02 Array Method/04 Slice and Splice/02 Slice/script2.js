eb20e51a57befdeee546c406ce6f516fe3cbbdbe
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-15 14:01:57 +0700

    Add files via upload

---
+++ 09 Array/02 Array Method/04 Slice and Splice/02 Slice/script2.js
@@ -0,0 +1,14 @@
+var cemilan1 = ['Basreng', 'Seblak', 'Serundeng Pedas', 'Makaroni', 'Kacang Atom'];
+
+// var cemilan2 = cemilan1.slice(0);
+// var cemilan2 = cemilan1.slice(3,5);
+// var cemilan2 = cemilan1.slice(-4);
+var cemilan2 = cemilan1.slice(-1);
+// var cemilan2 = cemilan1.slice(-4, -1);
+
+console.log(cemilan2.join(' - '));
+
+
+// Note :
+// Slice -> Returns Elements in Array, as a new array
+// slice(start, end);
