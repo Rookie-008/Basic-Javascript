612f518d0d285b4ab4965eebd08dcfac36d98ca5
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-16 21:03:04 +0700

    Add files via upload

---
+++ 10 Object/01 Create Object/01/script2.js
@@ -0,0 +1,10 @@
+var menu = {
+	name : "Jus",
+	price : 35,
+	type : ["Melon", "Alpukat", "Jambu", "Jeruk", "Wortel", "Mangga", "Pepaya", "Markisa", "Anggur", "Durian"],
+	address : {
+		Street : "Garden Street",
+		City : "Garden",
+		Contact : 123-456
+	}
+};
---
+++ 10 Object/01 Create Object/01/script3.js
@@ -0,0 +1,10 @@
+var menu = {
+	name : "Kacang",
+	price : 35,
+	type : ["Bawang", "Kedele", "Mente", "Kenari", "Almond"],
+	address : {
+		Street : "Garden Street",
+		City : "Garden",
+		Contact : 123-456
+	}
+};
