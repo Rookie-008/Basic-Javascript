eb50f8bf3e4909edf11ea5433752171cac34d297
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-16 21:08:40 +0700

    Add files via upload

---
+++ 11 JSON/01 Object to JSON/03/script2.js
@@ -0,0 +1,5 @@
+// Simple Array
+var juice = ['Jus Wortel', 'Jus Tomat', 'Jus Pepaya', 'Jus Melon', 'Jus Alpukat', 'Jus Strawberry', 'Jus Anggur', 'Jus Jeruk', 'Jus Jambu', 'Jus Mangga'];
+
+console.log(JSON.stringify(juice));
+console.log(JSON.stringify(juice[0]));
