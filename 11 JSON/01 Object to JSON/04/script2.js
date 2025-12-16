9540d369a0bc967207d25a671d258b08b11a8438
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-17 06:31:46 +0700

    Update script2.js

--- 11 JSON/01 Object to JSON/04/script2.js
+++ 11 JSON/01 Object to JSON/04/script2.js
@@ -1,14 +1,14 @@
 // Simple JSON
-var menu = {
-	name : "Charlie Russo",
+var data = {
+	name : "Bruce Lee",
 	address : "Garden Street",
 	point : [5,15,25,35,45,55,65,75]
 };
 
 // Simple Object to JSON
-// console.log(JSON.stringify(menu, null, 5));
-console.log(JSON.stringify(menu, null, '->'));
+// console.log(JSON.stringify(data, null, 5));
+console.log(JSON.stringify(data, null, '->'));
 
 
 // Access Array Values
-console.log(JSON.stringify(menu.point[5]));
+console.log(JSON.stringify(data.point[5]));
