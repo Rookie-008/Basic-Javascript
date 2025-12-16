f23024e61220e96a460694a8c82c11552cd00348
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-16 21:09:36 +0700

    Add files via upload

---
+++ 11 JSON/01 Object to JSON/05/script2.js
@@ -0,0 +1,39 @@
+// Array Object
+var cemilan = [
+		{
+			"name" : "Keripik Singkong",
+			"point" : [50, 70]
+
+		},
+
+		{
+			"name" : "Keripik Pisang",
+			"point" : [100, 90]
+		},
+		
+		{
+			"name" : "Keripik Kentang",
+			"point" : [50, 70]
+
+		},
+
+		{
+			"name" : "Keripik Sukun",
+			"point" : [100, 90]
+		}, 
+		
+		{
+			"name" : "Keripik Tempe",
+			"point" : [50, 70]
+
+		},
+
+		{
+			"name" : "Keripik Kulit",
+			"point" : [100, 90]
+		}, 
+
+];
+
+// console.log(JSON.stringify(data));
+console.log(JSON.stringify(data, 1));
