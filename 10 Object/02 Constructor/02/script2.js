d19e1b7c0a86b91ad879697bf3e27287a802d424
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-16 21:04:54 +0700

    Add files via upload

---
+++ 10 Object/02 Constructor/02/script2.js
@@ -0,0 +1,9 @@
+function Menu(name, price) {
+	// console.log(this);
+	// console.log("Jus Wortel");
+	this.name = name;
+	this.price = price;
+}
+
+var menu1 = new Menu('Kacang Kedele', 40);
+var menu2 = new Menu('Kacang Polong', 40);
